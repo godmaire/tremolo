cc99d2dec7c191402a9b4b192910f2ac4de898fd
Raine Godmaire <its@raine.ing>
2025-09-20 18:04:59 -0400

    misc: one chance at auth

--- src/server/agent.rs
+++ src/server/agent.rs
@@ -9,26 +9,59 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
-use tracing::{debug, info, warn};
+use tracing::{info, warn};
 
 use crate::server::SharedState;
 
-#[derive(Debug, thiserror::Error, Serialize, Deserialize)]
-enum Error {
-    #[error("invalid message type")]
-    InvalidMessage,
-}
-
 #[derive(Debug, Serialize, Deserialize)]
-enum ClientMessage {
+enum AgentMsg {
+    Ack,
     AuthRequest { name: String, token: String },
-    Ack,
+
+    Invalid,
+}
+
+impl From<AgentMsg> for ws::Message {
+    fn from(value: AgentMsg) -> Self {
+        let msg = serde_json::to_string(&value).expect("failed to serialize AgentMsg");
+        ws::Message::text(msg)
+    }
+}
+
+impl From<ws::Message> for AgentMsg {
+    fn from(value: ws::Message) -> Self {
+        let msg = match value.into_text() {
+            Ok(msg) => msg.to_string(),
+            Err(_) => return Self::Invalid,
+        };
+
+        serde_json::from_str(&msg).unwrap_or(Self::Invalid)
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-enum ServerMessage {
+enum ServerMsg {
     Ack,
-    Error(Error),
+
+    Invalid,
+}
+
+impl From<ServerMsg> for ws::Message {
+    fn from(value: ServerMsg) -> Self {
+        let msg = serde_json::to_string(&value).expect("failed to serialize ServerMsg");
+        ws::Message::text(msg)
+    }
+}
+
+impl From<ws::Message> for ServerMsg {
+    fn from(value: ws::Message) -> Self {
+        let msg = match value.into_text() {
+            Ok(msg) => msg.to_string(),
+            Err(_) => return Self::Invalid,
+        };
+
+        serde_json::from_str(&msg).unwrap_or(Self::Invalid)
+    }
 }
 
 /// ==================== WS /ws/agent ====================
@@ -45,6 +78,10 @@ async fn handle_socket(mut socket: WebSocket, state: Arc<SharedState>) {
         Some(name) => name,
         None => {
             warn!("Unauthorized agent attempted to connect");
+
+            // NOTE: We don't actually care at this point because we're closing the
+            // connection anyway
+            let _ = socket.send(ServerMsg::Invalid.into()).await;
             return;
         }
     };
@@ -52,54 +89,27 @@ async fn handle_socket(mut socket: WebSocket, state: Arc<SharedState>) {
     info!(name = agent_name, "Connected to agent.");
 }
 
-fn encode_message(msg: ServerMessage) -> ws::Message {
-    let msg = serde_json::to_string(&msg).expect("failed to serialize ServerMessage");
-    ws::Message::text(msg)
-}
-
-fn decode_message(msg: Result<ws::Message, axum::Error>) -> Option<ClientMessage> {
-    // Flatten all the options and results into options then propegate up the None values
-    // before deserializing the message into a message we understand
-    let msg = msg.ok()?.into_text().ok()?.to_string();
-    debug!(msg = msg, "Websocket message received");
-    serde_json::from_str(&msg).ok()
-}
-
 async fn handle_authentication(db: &PgPool, socket: &mut WebSocket) -> Option<String> {
-    loop {
-        let msg = socket.recv().await?;
-        let msg = match decode_message(msg) {
-            Some(msg) => msg,
-            None => {
-                socket
-                    .send(encode_message(ServerMessage::Error(Error::InvalidMessage)))
-                    .await
-                    .ok()?;
+    // If it's not the message we expect, then they fail
+    let msg = socket.recv().await?.ok()?.into();
+    let (name, token) = match msg {
+        AgentMsg::AuthRequest { name, token } => (name, token),
+        _ => return None,
+    };
 
-                continue;
-            }
-        };
-
-        // If it's not the message we expect, then we retry in the future
-        let (name, token) = match msg {
-            ClientMessage::AuthRequest { name, token } => (name, token),
-            _ => continue,
-        };
-
-        // We attempt to update the token's last_used timestamp, but, if there are no rows
-        // affected, we know that the token does not exist and the agent is not authenticated
-        //
-        // If one row was affected, then we return Some(agent_name)
-        let expected_rows_affected = 1;
-        return sqlx::query!(
-            "UPDATE agents_tokens SET last_used = CURRENT_TIMESTAMP WHERE token = $1",
-            token
-        )
-        .execute(db)
-        .await
-        .ok()?
-        .rows_affected()
-        .eq(&expected_rows_affected)
-        .then_some(name);
-    }
+    // We attempt to update the token's last_used timestamp, but, if there are no rows
+    // affected, we know that the token does not exist and the agent is not authenticated
+    //
+    // If one row was affected, then we return Some(agent_name)
+    let expected_rows_affected = 1;
+    sqlx::query!(
+        "UPDATE agents_tokens SET last_used = CURRENT_TIMESTAMP WHERE token = $1",
+        token
+    )
+    .execute(db)
+    .await
+    .ok()?
+    .rows_affected()
+    .eq(&expected_rows_affected)
+    .then_some(name)
 }
