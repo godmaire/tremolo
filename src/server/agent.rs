7c8b74065ba06ad7fa723b2b0b0488e910dee76f
Raine Godmaire <its@raine.ing>
2025-09-20 21:40:45 -0400

    feat: use http headers for websocket auth

--- src/server/agent.rs
+++ src/server/agent.rs
@@ -3,20 +3,23 @@ use std::{fmt::Debug, sync::Arc};
 use axum::{
     extract::{
         State, WebSocketUpgrade,
-        ws::{self, WebSocket},
+        ws::{self, Message, WebSocket},
     },
+    http::{HeaderMap, StatusCode},
     response::IntoResponse,
 };
+use futures_util::stream::{SplitSink, SplitStream, StreamExt};
 use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
-use tracing::{info, warn};
+use tracing::info;
 
 use crate::server::SharedState;
+
+static TREMOLO_AUTH_HEADER_KEY: &str = "X-Tremolo-Auth";
+static TREMOLO_AGENT_NAME_HEADER_KEY: &str = "X-Tremolo-Agent-Name";
 
 #[derive(Debug, Serialize, Deserialize)]
 enum AgentMsg {
     Ack,
-    AuthRequest { name: String, token: String },
 
     Invalid,
 }
@@ -67,49 +70,65 @@ impl From<ws::Message> for ServerMsg {
 /// ==================== WS /ws/agent ====================
 pub(crate) async fn connect_agent(
     ws: WebSocketUpgrade,
+    headers: HeaderMap,
     State(state): State<Arc<SharedState>>,
-) -> impl IntoResponse {
-    let state = state.clone();
-    ws.on_upgrade(|socket| handle_socket(socket, state))
-}
+) -> Result<impl IntoResponse, StatusCode> {
+    let auth_token = headers
+        .get(TREMOLO_AUTH_HEADER_KEY)
+        .ok_or(StatusCode::FORBIDDEN)?
+        .to_str()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
 
-async fn handle_socket(mut socket: WebSocket, state: Arc<SharedState>) {
-    let agent_name = match handle_authentication(&state.db, &mut socket).await {
-        Some(name) => name,
-        None => {
-            warn!("Unauthorized agent attempted to connect");
-
-            // NOTE: We don't actually care at this point because we're closing the
-            // connection anyway
-            let _ = socket.send(ServerMsg::Invalid.into()).await;
-            return;
-        }
-    };
-
-    info!(name = agent_name, "Connected to agent.");
-}
-
-async fn handle_authentication(db: &PgPool, socket: &mut WebSocket) -> Option<String> {
-    // If it's not the message we expect, then they fail
-    let msg = socket.recv().await?.ok()?.into();
-    let (name, token) = match msg {
-        AgentMsg::AuthRequest { name, token } => (name, token),
-        _ => return None,
-    };
+    let agent_name = headers
+        .get(TREMOLO_AGENT_NAME_HEADER_KEY)
+        .ok_or(StatusCode::BAD_REQUEST)?
+        .to_str()
+        .map_err(|_| StatusCode::BAD_REQUEST)?
+        .to_string();
 
     // We attempt to update the token's last_used timestamp, but, if there are no rows
     // affected, we know that the token does not exist and the agent is not authenticated
     //
     // If one row was affected, then we return Some(agent_name)
     let expected_rows_affected = 1;
-    sqlx::query!(
+    let is_authorized = sqlx::query!(
         "UPDATE agents_tokens SET last_used = CURRENT_TIMESTAMP WHERE token = $1",
-        token
+        auth_token
     )
-    .execute(db)
+    .execute(&state.db)
     .await
-    .ok()?
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
     .rows_affected()
-    .eq(&expected_rows_affected)
-    .then_some(name)
+    .eq(&expected_rows_affected);
+
+    if !is_authorized {
+        return Err(StatusCode::FORBIDDEN);
+    }
+
+    // Create the agent in the database
+    sqlx::query!("INSERT INTO agents (name) VALUES ($1)", agent_name)
+        .execute(&state.db)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    // Upgrade the websocket!!
+    let state = state.clone();
+    Ok(ws.on_upgrade(|socket| handle_socket(socket, state, agent_name)))
+}
+
+async fn handle_socket(socket: WebSocket, _state: Arc<SharedState>, agent_name: String) {
+    info!(name = agent_name, "Connected to agent.");
+
+    let (sender, receiver) = socket.split();
+
+    tokio::spawn(handle_sender(sender));
+    tokio::spawn(handle_receiver(receiver));
+}
+
+async fn handle_sender(_sender: SplitSink<WebSocket, Message>) {
+    todo!()
+}
+
+async fn handle_receiver(_receiver: SplitStream<WebSocket>) {
+    todo!()
 }
